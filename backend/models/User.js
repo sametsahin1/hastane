64c7248351679084e06161e6eb1981d6a8e98c93
sametsahin1 <a.samed.sahinn@gmail.com>
2025-01-20 05:32:32 +0300

    resimler geliyor

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   passwordHash: { type: String, required: true },
-  role: { type: String, default: 'editor' }, // "admin" veya "editor" vs.
+  role: { type: String, default: 'editor' }, // "admin" veya "
   createdAt: { type: Date, default: Date.now }
 });
 
