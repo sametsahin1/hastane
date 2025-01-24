b773608f9c8f40de0454fdcb6b56fcf27d9bb3c9
sametsahin1 <a.samed.sahinn@gmail.com>
2025-01-24 16:30:42 +0300

    ..

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Nginx servisini durdur
+systemctl stop nginx
+
 # GitHub'dan güncellemeleri çek
 git pull origin main
 
@@ -7,3 +10,10 @@ git pull origin main
 docker-compose down
 docker-compose build --no-cache
 docker-compose up -d
+
+# Eğer container'lar başarıyla başlatılamazsa Nginx'i tekrar başlat
+if [ $? -ne 0 ]; then
+    echo "Docker container'ları başlatılamadı. Nginx'i tekrar başlatıyorum..."
+    systemctl start nginx
+    exit 1
+fi
